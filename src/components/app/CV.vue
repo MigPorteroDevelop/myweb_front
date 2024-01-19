1dd72890a24f487e3f38d0ce1aad3d5e529db518
Miguel Portero Ruiz <migporuiz@gmail.com>
2024-01-19 10:29:19 +0100

    CV 2

--- src/components/app/CV.vue
+++ src/components/app/CV.vue
@@ -9,7 +9,8 @@ export default {
         return {
             pdf1: 'CV-EN.pdf',
             pdf2: 'CV-DE.pdf',
-            route1: '/documents/CV-EN',
+            route1: '../../src/assets/documents/CV-EN.pdf',
+            /**Por qué funciona en una carpeta y en la otra no? */
             route2: '/documents/CV-DE',
         };
     }
@@ -17,12 +18,13 @@ export default {
 </script>
 
 <template>
-    <div class="flex flex-col sm:flex-row justify-center items-center space-y-10 sm:space-y-0 sm:space-x-28 py-32">
-        <div class="w-52">
-            <atropos-component shadow-scale="0">
-                <img src="/images/flag-EN.png" />
-                <h1 class="text-lg text-center font-bold pt-6 pb-2">{{ $t("impressum.urheberrecht") }}</h1>
-                <a :href="route1" :download="pdf1" class="
+    <div class="flex flex-col sm:flex-row justify-center items-center space-y-10 sm:space-y-0 sm:space-x-18 py-32">
+        <div class="sm:hover:border-2 sm:hover:border-blue">
+            <div class="w-52 sm:w-72 sm:p-10 sm:hover:bg-light_blue">
+                <atropos-component shadow-scale="0">
+                    <img src="/images/flag-EN.png" />
+                    <h1 class="text-lg text-center font-bold pt-6 pb-2">{{ $t("impressum.urheberrecht") }}</h1>
+                    <a :href="route1" :download="pdf1" class="
                     block
                     py-2
                     px-4
@@ -33,14 +35,16 @@ export default {
                     duration-300
                     hover:text-blue
                     cursor-pointer">
-                    <em>{{ $t("impressum.urheberrecht2") }}</em></a>
-            </atropos-component>
+                        <em>{{ $t("impressum.urheberrecht2") }}</em></a>
+                </atropos-component>
+            </div>
         </div>
-        <div class="w-52">
-            <atropos-component shadow-scale="0">
-                <img src="/images/flag-DE.png" />
-                <h1 class="text-lg text-center font-bold pt-6 pb-2">{{ $t("impressum.urheberrecht3") }}</h1>
-                <a :href="route2" :download="pdf2" class="
+        <div class="sm:hover:border-2 sm:hover:border-blue">
+            <div class="w-52 sm:w-72 sm:p-10 sm:hover:bg-light_blue">
+                <atropos-component shadow-scale="0">
+                    <img src="/images/flag-DE.png" />
+                    <h1 class="text-lg text-center font-bold pt-6 pb-2">{{ $t("impressum.urheberrecht3") }}</h1>
+                    <a :href="route2" :download="pdf2" class="
                     block
                     py-2
                     px-4
@@ -51,8 +55,9 @@ export default {
                     duration-300
                     hover:text-blue
                     cursor-pointer">
-                    <em>{{ $t("impressum.urheberrecht2") }}</em></a>
-            </atropos-component>
+                        <em>{{ $t("impressum.urheberrecht2") }}</em></a>
+                </atropos-component>
+            </div>
         </div>
     </div>
 </template>
