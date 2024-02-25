ab351ef70aca206a6fe449ea7b52fc931b35db47
Miguel Portero Ruiz <58771720+MigPorteroDevelop@users.noreply.github.com>
2024-02-26 00:27:36 +0100

    Contact form

--- src/components/inputs/Input.vue
+++ src/components/inputs/Input.vue
@@ -1,64 +1,62 @@
-<script>
+<script setup>
+import { ref } from "vue";
 import axios from "axios";
 import { BASE_URL } from '@/main.js';
 
+const form = ref({});
 
-async function onClickCaptcha(e) {
-  e.preventDefault();
-  grecaptcha.ready(function() {
-    grecaptcha.execute('6LeDmX4pAAAAAIOBNIit7YtMC0jvrQ08SUHyQRk7', {action: 'submit'}).then(function(token) {
-        return token;
-    });
+const onClickCaptcha = async function () {
+  return grecaptcha.execute('6LeDmX4pAAAAAIOBNIit7YtMC0jvrQ08SUHyQRk7', { action: 'submit' }).then(function (token) {
+    return token;
   });
 }
 
-export default {
-  data() {
-    return {
-      errorMessage: "",
-      formDisabled: true,
-    };
-  },
-  props: {
-    task: Object,
-  },
-  methods: {
-    handleSuccess(code) {
-      this.errorMessage = "";
+const submitForm = async function () {
+  const responseCaptcha = await onClickCaptcha();
 
-    },
-    async submitForm() {
-      const responseCaptcha = await onClickCaptcha()
+  if (responseCaptcha) {
 
-      if (responseCaptcha) {
-        try {
-          const response = await axios.post(
-            `${BASE_URL}/api/contact`,
-            this.task
-          );
-        } catch {
-          
-        }
-      } else {
-        this.errorMessage = alert(this.$t("recaptcha.r1"));
-
+    const response = await axios.post(
+      `${BASE_URL}/sendMail.php`,
+      {
+        ...form.value,
+        captcha: responseCaptcha,
       }
-    },
-  },
+    );
+
+    console.log(response);
+
+    if (!response.response) {
+      alert(response.messageResponse)
+    }
+
+  }
 };
+
+// ejemplo de php a validar
+// $_POST['name']
+// $_POST['email']
+// $_POST['message']
+
+
+// echo json_encode([
+//   'response'=> false,
+//   'messageRespose'=> 'El mail no es valido'
+// ]);
+
 </script>
 
 <template>
   <div>
     <label for="name" class="block mb-2 text-sm font-medium">Name</label>
-    <input type="text" id="name" v-model.trim="task.name"
+    <input type="text" id="name" v-model.trim="form.name"
       class="shadow-2xl border text-sm rounded-lg block w-72 md:w-full p-3" required />
   </div>
   <div>
     <label for="email" class="block mb-2 text-sm font-medium">{{
       $t("home.email")
     }}</label>
-    <input type="email" id="email" v-model.trim="task.email" class="
+    <input type="email" id="email" v-model.trim="form.email" class="
         block
         p-3
         w-72
@@ -72,7 +70,7 @@ export default {
   </div>
   <div class="md:col-span-2">
     <label for="message" class="block mb-2 text-sm font-medium">{{ $t("home.message") }}</label>
-    <textarea type="text" id="message" v-model.trim="task.message" rows="6" class="
+    <textarea type="text" id="message" v-model.trim="form.message" rows="6" class="
       block
         p-3
         w-72
