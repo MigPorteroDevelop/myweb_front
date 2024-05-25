10f282513e317244b5a248fba053dbd11be8f01a
Miguel Portero Ruiz <58771720+MigPorteroDevelop@users.noreply.github.com>
2024-05-25 18:07:45 +0200

    Categories filtered

--- src/main.js
+++ src/main.js
@@ -4,8 +4,6 @@ import router from "./router";
 import './assets/tailwind.css';
 import i18n from "./locales/i18n";
 import locales from "./locales/locales"
-import VueSplide from '@splidejs/vue-splide';
-import VueCookies from 'vue-cookies';
 import PrimeVue from 'primevue/config';
 import 'atropos/css';
 import 'primevue/resources/themes/lara-light-green/theme.css';
@@ -13,7 +11,7 @@ import 'primevue/resources/primevue.min.css';
 import '@/assets/normalize.css'
 
 
-createApp(App).use(router).use(i18n).use(locales).use(VueSplide).use(VueCookies).use(PrimeVue).mount('#app')
+createApp(App).use(router).use(i18n).use(locales).use(PrimeVue).mount('#app')
 
 // url-base
 export const BASE_URL = 'http://localhost:8000';
--- tailwind.config.js
+++ tailwind.config.js
@@ -17,7 +17,17 @@ module.exports = {
       gray: '#969db2',
       blue: '#4d6df7',
       light_blue: '#f8f9ff',
-      dark_blue: '#090e34'
+      dark_blue: '#090e34',
+
+      grey: '#f2f2f2',
+      beige: '#f5f5dc',
+      softPink: '#f9c4d2',
+      softBlue: '#c4d8e2',
+      softGreen: '#d6e9d4',
+      blueGrey: '#a8b8c8'
     },
   },
+  plugins: [
+    require('@tailwindcss/aspect-ratio')
+  ],
 }
