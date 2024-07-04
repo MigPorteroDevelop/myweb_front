725e58a877d898e784b4235c55d924ef110e3b7b
Miguel Portero Ruiz <58771720+MigPorteroDevelop@users.noreply.github.com>
2024-07-04 19:17:39 +0200

    projects adjustments

--- src/data/projects.js
+++ src/data/projects.js
@@ -16,35 +16,5 @@ export const db = [
     web: "https://psychology-patients.netlify.app/",
     github: "https://github.com/MigPorteroDevelop/Psychology-patients",
     category: [1],
-  },
-  {
-    id: 3,
-    name: "Project 3",
-    imageSrc:
-      "https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg",
-    imageAlt: "dream-guitar",
-    web: "https://silly-druid-8f86ca.netlify.app/",
-    github: "https://github.com/MigPorteroDevelop/Guitar-Ecommerce",
-    category: [3, 2],
-  },
-  {
-    id: 4,
-    name: "Project 3",
-    imageSrc:
-      "https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg",
-    imageAlt: "dream-guitar",
-    web: "https://silly-druid-8f86ca.netlify.app/",
-    github: "https://github.com/MigPorteroDevelop/Guitar-Ecommerce",
-    category: [3, 2],
-  },
-  {
-    id: 5,
-    name: "Project 3",
-    imageSrc:
-      "https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg",
-    imageAlt: "dream-guitar",
-    web: "https://silly-druid-8f86ca.netlify.app/",
-    github: "https://github.com/MigPorteroDevelop/Guitar-Ecommerce",
-    category: [3, 2],
-  },
+  }
 ];
